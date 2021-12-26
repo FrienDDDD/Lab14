86396867bc3e6787d8fc91299d84c6022cb7ee88
FrienDDDD <friendjunk@gmail.com>
2021-12-27 05:30:22 +0700

    lab14_1.cpp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,36 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int n,double b[]){
+
+for(int i = 0;i < n;i++){
+    b[0] += a[i]/n ;
+}
+double sum = 0 ,Mean = b[0]; 
+for(int i = 0;i < n;i++){
+    sum += pow(a[i]-Mean,2) ;
+    b[1] = sqrt(sum/n) ;
+}
+b[2] = 1 ;
+for(int i = 0;i < n;i++){
+    b[2] *= pow(a[i],1.0/n) ;
+}  
+double sum1 = 0 ;
+for(int i = 0;i < n;i++){
+    sum1 += (1.0/a[i]) ;
+    b[3] = n/sum1 ;
+}
+double Max = a[0],Min = a[0] ;
+for(int i = 0;i < n;i++){
+    if(a[i] > Max){
+        Max = a[i] ;
+        b[4] = a[i] ;
+    }if(a[i] < Min){
+        b[5] = a[i] ;
+    }
+}
+
+}
+
+
